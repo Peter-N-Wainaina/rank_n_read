c1fd738f19f24251b1796d9686b2f59b8ae177a2
Mnumzana Moyo <mfm222@cornell.edu>
2025-03-21 14:48:24 -0400

    Remove test dataset declaration

--- backend/tests/test_dataset.py
+++ backend/tests/test_dataset.py
@@ -2,10 +2,8 @@ import pytest
 import os 
 
 from backend.dataset import Dataset
-from backend.config import ROOT_PATH
+from test_constants import BOOKS_TEST_JSON
 
-
-BOOKS_TEST_JSON = os.path.join(ROOT_PATH,'tests', 'test_data','books_test.json')
 
 @pytest.fixture
 def dataset() -> Dataset:
